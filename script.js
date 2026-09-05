a40b5f77c2ca4911cbd17fec18d343cfc6ed6d3f
Sofia Carvajal <lorensofian23@gmail.com>
2026-09-04 23:00:43 -0500

    style: ordena estilos del portafolio

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const menuToggle = document.querySelector('.menu-toggle');
 const siteNav = document.querySelector('#site-nav');
 
 menuToggle.addEventListener('click', () => {
-  const isOpen = siteNav.classList.toggle('is-open');
-  menuToggle.setAttribute('aria-expanded', String(isOpen));
+    const isOpen = siteNav.classList.toggle('is-open');
+    menuToggle.setAttribute('aria-expanded', String(isOpen));
 });
 
 siteNav.addEventListener('click', (event) => {
-  if (event.target.matches('a')) {
-    siteNav.classList.remove('is-open');
-    menuToggle.setAttribute('aria-expanded', 'false');
-  }
+    if (event.target.matches('a')) {
+        siteNav.classList.remove('is-open');
+        menuToggle.setAttribute('aria-expanded', 'false');
+    }
 });
